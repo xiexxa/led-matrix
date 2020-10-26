c3d67fa74b2a656026b1ab9911cc58778f997006
eisuke <eisuk4@gmail.com>
2020-10-26 20:47:22 +0900

    他の環境でもビルド可能なことを確認

--- config/index.js
+++ config/index.js
@@ -13,7 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '172.19.210.25', // can be overwritten by process.env.HOST
+    //host: '172.19.210.25', // can be overwritten by process.env.HOST
+    host: '192.168.0.13', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
