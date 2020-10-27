44a124084b31336861e8f22028abb508d6057455
eisuke <eisuk4@gmail.com>
2020-10-27 17:53:47 +0900

    Sendボタンを押すとパネルにテキストが表示される

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const async = require('async');
+const LedMatrix = require("easybotics-rpi-rgb-led-matrix");
+let matrix;
+let fontpath;
 
 async function main() {
+    matrix = new LedMatrix(16, 32, 1, 3, 50, 'adafruit-hat' );
+    fontpath =  path.resolve(__dirname, '..')+'/fonts/'+'ufo.bdf';
+    
     exports.index = function(req, res) {
         /*
         res.json({
             message: "api"
         });
         */
-       console.log(req);
-       res.send(req.body.text);
+       let text = req.body.text;
+
+       matrix.clear();
+       matrix.drawText(0, 0, text, fontpath, 0, 255, 0);
+       matrix.update();
+       res.send(text);
     }
 }
 main();
