0103e6bdfa8a5d16e8ce0a1c7da99ba36fb05610
eisuke <eisuk4@gmail.com>
2020-10-27 18:00:26 +0900

    テキストを入力したら、スクロールするようにした

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,22 +4,32 @@ const LedMatrix = require("easybotics-rpi-rgb-led-matrix");
 let matrix;
 let fontpath;
 
+async function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
 async function main() {
     matrix = new LedMatrix(16, 32, 1, 3, 50, 'adafruit-hat' );
     fontpath =  path.resolve(__dirname, '..')+'/fonts/'+'ufo.bdf';
-    
-    exports.index = function(req, res) {
+
+    exports.index = async function(req, res) {
         /*
         res.json({
             message: "api"
         });
         */
        let text = req.body.text;
+       let x = 0;
 
-       matrix.clear();
-       matrix.drawText(0, 0, text, fontpath, 0, 255, 0);
-       matrix.update();
        res.send(text);
+       while (x <= 96) {
+        matrix.clear();
+        matrix.drawText(x, 0, text, fontpath, 0, 255, 0);
+        matrix.update();
+        x++;
+        await sleep(100);
+       }
+       console.log('done: '+text);
     }
 }
 main();
