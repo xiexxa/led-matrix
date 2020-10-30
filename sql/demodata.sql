bcc0d2bb5c5b8afddeea980a5cdb30c95cfb1722
eisuke <eisuk4@gmail.com>
2020-10-30 14:14:36 +0900

    text_historiesテーブルにデモのデータを追加した。

---
+++ sql/demodata.sql
@@ -0,0 +1,3 @@
+insert into text_histories(body, created_at, updated_at) values ('TEXT TEXT', '2020-10-31 0:00:00', '2020-10-31 0:00:00');
+insert into text_histories(body, created_at, updated_at) values ('TEXT TEXT 2', '2020-10-31 0:00:00', '2020-10-31 0:00:00');
+insert into text_histories(body, created_at, updated_at) values ('TEXT TEXT 3', '2020-10-31 0:00:00', '2020-10-31 0:00:00');
