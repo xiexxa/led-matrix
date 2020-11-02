06479daed7208f21ba5d85145d38b383eb206e7b
eisuke <eisuk4@gmail.com>
2020-11-02 18:09:31 +0900

    RSS feedsテーブルを追加

--- sql/init.sql
+++ sql/init.sql
@@ -4,3 +4,5 @@ drop table if exists text_histories;
 create table text_histories (id int not null primary key auto_increment, body varchar(10000), created_at datetime, updated_at datetime);
 -- /text/phraseの定型文
 create table phrases (id int not null primary key auto_increment, body varchar(10000) not null, created_at datetime, updated_at datetime);
+-- RSSの登録テーブル
+create table feeds ( id int not null primary key auto_increment, name varchar(100) not null, url varchar(1000) not null, created_at datetime not null, updated_at datetime not null );
